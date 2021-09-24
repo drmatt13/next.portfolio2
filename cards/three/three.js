075acbda9f7deac71b6d724f25f157551447b1b5
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2021-09-24 03:33:27 -0400

    py

--- cards/three/three.js
+++ cards/three/three.js
@@ -12,12 +12,29 @@ export default [
   <canvas id="c"></canvas>
 </body>
 </html>`,
+"css": `
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  height: 100vh;
+}
+
+#c {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+`,
 "javascript three": `
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/build/three.module.js';
 
 function main() {
   const canvas = document.querySelector('#c');
-  const renderer = new THREE.WebGLRenderer({canvas});
+  const renderer = new THREE.WebGLRenderer({canvas, alpha: true, antialias: true});
 
   const fov = 75;
   const aspect = 2;  // the canvas default
@@ -28,39 +45,73 @@ function main() {
 
   const scene = new THREE.Scene();
 
-  // light
-  const color = 0xFFFFFF;
-  const intensity = 1;
-  const light = new THREE.DirectionalLight(color, intensity);
-  light.position.set(-1, 2, 4);
-  scene.add(light);
+  {
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+  }
 
   const boxWidth = 1;
   const boxHeight = 1;
   const boxDepth = 1;
   const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-  const material = new THREE.MeshPhongMaterial({color: 0x44aa88});  // greenish blue
+  function makeInstance(geometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({color});
 
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    cube.position.x = x;
+
+    return cube;
+  }
+
+  const cubes = [
+    makeInstance(geometry, 0x44aa88,  0),
+    // makeInstance(geometry, 0x8844aa, -2),
+    // makeInstance(geometry, 0xaa8844,  2),
+  ];
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
 
   function render(time) {
-    time *= 0.001;  // convert time to seconds
+    time *= 0.001;
 
-    cube.rotation.x = time;
-    cube.rotation.y = time;
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * .1;
+      const rot = time * speed;
+      cube.rotation.x = rot;
+      cube.rotation.y = rot;
+    });
 
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
   }
-  requestAnimationFrame(render);
 
+  requestAnimationFrame(render);
 }
 
 main();
 `,
-"render": ["html", "javascript three"]
+"render": ["html", "css", "javascript three"]
 }
 ]
---
+++ update.py
@@ -0,0 +1,11 @@
+from os import listdir
+from os.path import isfile, join
+
+apps = listdir('./apps')
+notes = listdir('./notes')
+cards = []
+for 
+
+for app in apps
+
+
