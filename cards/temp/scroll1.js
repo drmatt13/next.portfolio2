08881113d8d3913d1bf1cc709e53787884cf4fb4
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2021-09-25 23:56:38 -0400

    9/25

--- cards/temp/scroll1.js
+++ cards/temp/scroll1.js
@@ -5,8 +5,14 @@ export default [
   <div id="overflow"></div>
 </div>
 <div class="container">
-  <div>x: <span id="x">0.00</span></div>
-  <div>y: <span id="y">0.00</span></div>
+  <div>
+    <div>x: <span id="x1">0</span></div>
+    <div>y: <span id="y1">0</span></div>
+  </div>
+  <div>
+    <div>x: <span id="x2">0</span>%</div>
+    <div>y: <span id="y2">0</span>%</div>
+  </div>
 </div>
 `,
 'css': `
@@ -17,33 +23,53 @@ body {
 .container:first-of-type {
   overflow: auto;
   height: 200px;
-  width: 200px;
+  width: 300px;
 }
 
 #overflow {
   background: rgb(255,186,75);
   background: radial-gradient(circle, rgba(255,186,75,1) 0%, rgba(51,43,51,1) 80%, rgba(116,149,126,1) 100%);
-  height: 1000px;
-  width: 1000px;
+  height: 900px;
+  width: 1200px;
 }
 
 .container:nth-child(2) {
+  margin-left: 10px;
+}
+
+.container:nth-child(2) > div:first-of-type, 
+.container:nth-child(2) > div:last-of-type {
   background-color: #fff8;
   align-self: flex-start;
-  font-size: large;
+  font-size: xx-large;
+  margin-bottom: 10px;
   border-radius: 18px;
-  margin-left: 10px;
-  min-width: 100px;
+  min-width: 120px;
   padding: 10px;
-}`,
+}
+`,
 'javascript': `
-const x = document.getElementById("x")
-const y = document.getElementById("y")
+// pixels
+const x1 = document.getElementById("x1")
+const y1 = document.getElementById("y1")
+// percentage
+const x2 = document.getElementById("x2")
+const y2 = document.getElementById("y2")
+// containers
 const container = document.querySelector(".container")
+const overflow = document.getElementById("overflow")
+
+// change in size between parent and child containers
+const dx = overflow.clientWidth - container.clientWidth
+const dy = overflow.clientHeight - container.clientHeight
+
+const percentage = (partialValue, totalValue) => (100 * partialValue) / totalValue
 
 container.addEventListener('scroll', () => {
-  x.innerHTML = container.scrollLeft.toFixed(2)
-  y.innerHTML = container.scrollTop.toFixed(2)
+  x1.innerHTML = container.scrollLeft.toFixed(0)
+  y1.innerHTML = container.scrollTop.toFixed(0)
+  x2.innerHTML = percentage(container.scrollLeft, dx).toFixed(0)
+  y2.innerHTML = percentage(container.scrollTop, dy).toFixed(0)
 })
 `,
 'render': ["html", "css", "javascript"]
