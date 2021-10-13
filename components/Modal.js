5e2eeedfabf0229b78859e3adcd634d7bc9474a6
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2021-10-12 21:07:55 -0400

    10/12

--- components/Modal.js
+++ components/Modal.js
@@ -17,12 +17,10 @@ const Modal = ({ modal, toggleModal }) => {
   return <>
     <style global="true">{`
       .modal {
-        height: 65vh;
         min-height: 400px;
+        height: 65vh;
         max-height: 500px;
-        min-width: 300px;
-        width: 60vw;
-        max-width: 800px;
+        width: clamp(300px, 60vw, 800px);
       }
       ${!mobile && `
         .modal > ::-webkit-scrollbar {
@@ -76,11 +74,8 @@ const Modal = ({ modal, toggleModal }) => {
           background-color: rgba(0, 0, 0, 0)
         }
       `}
-      @media only screen and (max-width: 768px) {
-        .modal { width: 75vw }
-      }
-      @media only screen and (max-width: 640px) {
-        .modal { width: 92.5vw }
+      @media only screen and (max-width: 1023px) {
+        .modal { width: clamp(0px, 92.5vw, 500px) }
       }
       @media only screen and (max-height: 441px) {
         .modal { min-height: 90% }
@@ -97,13 +92,6 @@ const Modal = ({ modal, toggleModal }) => {
         background: -webkit-linear-gradient(to right, #0b879380, #944a9080);
         background: linear-gradient(to right, #0b879380, #944a9080);
       }
-      .mx-2 {
-        height: clamp(1.25em, 3.5vh, 1.5em);
-        width: clamp(1.25em, 3.5vh, 1.5em);
-      }
-      i {
-        font-size: clamp(.75em, 2vh, 1em);
-      }
     `}</style>
     <div className="animate-fade-in absolute overflow-x-hidden top-0 left-0 h-screen w-full flex justify-center items-center">
       <div 
@@ -111,7 +99,7 @@ const Modal = ({ modal, toggleModal }) => {
         className="absolute bg-black bg-opacity-25 h-screen w-screen"
       />
       <div className="modal relative overflow-y-hidden flex flex-col rounded-2xl shadow-lg bg-gray-300 dark:bg-gray-700 bg-opacity-95 dark:bg-opacity-90 transition-colors">
-        <div className="select-none flex-shrink-0 bg-gray-500 bg-opacity-20 w-full flex items-center">
+        <div className="select-none flex-shrink-0 bg-gray-500 bg-opacity-25 w-full flex items-center">
           <div className="flex-1 flex h-full overflow-y-hidden text-xs xs:text-sm sm:text-base">
             {["apps", "notes", "contact", "about"].map((m, key) => (
               <div
@@ -124,22 +112,19 @@ const Modal = ({ modal, toggleModal }) => {
                   justify-center 
                   items-center 
                   h-full
-                bg-white 
-                  bg-opacity-10 
-                  hover:bg-opacity-30 
+                  bg-gray-500 
+                  bg-opacity-25 
+                  hover:bg-white
+                  hover:text-white
+                  hover:bg-opacity-25
+                  hover:drop-shadow-2xl
                   cursor-pointer
                   transition-all
                 `}
               >
-                {m}
+                { m }
               </div>
             ))}
-          </div>
-          <div 
-            onClick={()=>toggleModal(null)}
-            className="mx-2 flex justify-center items-center rounded-full bg-gray-800 dark:bg-gray-200 text-gray-200 dark:text-black bg-opacity-80 hover:bg-red-500 dark:hover:bg-red-500 hover:cursor-pointer transition-colors duration-150 ease-out"
-          >
-            <i className="fas fa-times flex justify-center items-center" />
           </div>
         </div>
         {modal === 'apps' && <DynamicAppModal toggleModal={toggleModal} />}
