0925bb9d4929f14d059e0383f0a850a6c2b09c98
drmatt13 <58347724+drmatt13@users.noreply.github.com>
2021-10-22 21:35:26 -0400

    10/22

--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,7 @@ export default function Home() {
           </div>
         </div>
         <div className="flex-1 hidden sm:flex justify-center">
-          <MirrorIcons />
+          {/* <MirrorIcons /> */}
         </div>
       </div>
 
